8a676914a2bb0a2def1c6df981f63a37b3c4428c
leitelukz <lucasleite_123@yahoo.com.br>
2026-03-22 23:47:07 -0300

    Atualizar o ula.c
    Adicionado log e criação do arquivo de log. Além da finalização da leitura/escrita desse arquivo.

--- etapa1/src/ula/ula.c
+++ etapa1/src/ula/ula.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // 🟢 PASSO 5 — Função da ULA
 int ula(int A, int B, int F0, int F1, int ENA, int ENB, int INVA, int INC, int *carry) {
@@ -49,18 +50,34 @@ int main() {
 
     FILE *file = fopen("programa_etapa1.txt", "r");
 
+    FILE *log_file = fopen("saida_etapa1.txt", "w");
+
     if (file == NULL) {
-        printf("Erro ao abrir o arquivo\n");
+        printf("Erro ao abrir o arquivo de entrada\n");
+        return 1;
+    }
+    if (log_file == NULL) {
+        printf("Erro ao criar o arquivo de log\n");
+        fclose(file);
         return 1;
     }
 
     char linha[10];
 
     // 🟢 Valores iniciais (não interfere nos demais)
+
     int A = -1;
     int B = 1;
+    int PC = 0;
 
     while (fgets(linha, sizeof(linha), file)) {
+        
+    linha[strcspn(linha, "\n")] = 0;
+
+        if(strlen(linha) == 0) continue; 
+
+        char IR[10];
+        strcpy(IR, linha);
 
         int F0 = linha[0] - '0';
         int F1 = linha[1] - '0';
@@ -69,19 +86,27 @@ int main() {
         int INVA = linha[4] - '0';
         int INC = linha[5] - '0';
 
-        printf("Instrucao: %s", linha);
-        printf("F0=%d F1=%d ENA=%d ENB=%d INVA=%d INC=%d\n",
-               F0, F1, ENA, ENB, INVA, INC);
-
         // 🟢 PASSO 6 — Chamada da ULA
         int carry;
         int S = ula(A, B, F0, F1, ENA, ENB, INVA, INC, &carry);
 
+        printf("Cycle (PC): %d | IR: %s | A: %d | B: %d | S: %d | Carry: %d\n", PC, IR, A, B, S, carry);
         
-        printf("Resultado (S): %d\n", S);
-        printf("Carry: %d\n\n", carry);
+        fprintf(log_file, "Cycle: %d\n", PC);
+        fprintf(log_file, "PC: %d\n", PC);
+        fprintf(log_file, "IR: %s\n", IR);
+        fprintf(log_file, "A: %d\n", A);
+        fprintf(log_file, "B: %d\n", B);
+        fprintf(log_file, "S: %d\n", S);
+        fprintf(log_file, "Carry: %d\n", carry);
+        fprintf(log_file, "----------------------\n");
+
+        PC++;
     }
 
     fclose(file);
+    fclose(log_file);
+    
+    printf("\nProcessamento concluído. Verifique o arquivo 'saida_etapa1.txt'.\n");
     return 0;
 }
